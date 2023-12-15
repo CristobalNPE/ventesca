bbedc43b7c152f9d8867afea8da3926a0d2cc064
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-12-14 22:13:54 -0300

    Refactor inventory new page

--- app/routes/system+/inventory_.new.tsx
+++ app/routes/system+/inventory_.new.tsx
@@ -1,65 +1,27 @@
-import { SearchBar } from '#app/components/SearchBar.tsx'
-import { Stepper } from '#app/components/ui/stepper.tsx'
+import { Input } from '#app/components/ui/input.tsx'
+import { SelectModal } from '#app/components/ui/select-modal.tsx'
 import { prisma } from '#app/utils/db.server.ts'
 import { calculateDV } from '#app/utils/misc.tsx'
 import { LoaderFunctionArgs, json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { useState } from 'react'
 import { ItemEditor, action } from './__item-editor.tsx'
-import { SelectModal } from '#app/components/ui/select-modal.tsx'
 
-// export async function loader({ request }: LoaderFunctionArgs) {
-// 	const url = new URL(request.url)
-// 	const providerSearch = url.searchParams.get('provider')
-// 	const categorySearch = url.searchParams.get('category')
-
-// 	if (providerSearch) {
-// 		const providers = await prisma.provider.findMany({
-// 			select: { id: true, rut: true, name: true, fantasyName: true },
-// 			where: { rut: { contains: providerSearch } },
-// 		})
-
-// 		const categories = null
-// 		return json({ providers, categories })
-// 	}
-
-// 	if (categorySearch) {
-// 		const categories = await prisma.family.findMany({
-// 			select: { id: true, code: true, description: true },
-// 			where: { description: { contains: categorySearch } },
-// 		})
-// 		const providers = null
-// 		return json({ categories, providers })
-// 	}
-
-// 	return json({ providers: null, categories: null })
-// }
+import { Button } from '#app/components/ui/button.tsx'
+import { Icon } from '#app/components/ui/icon.tsx'
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const url = new URL(request.url)
-	const providerSearch = url.searchParams.get('provider')
-	const categorySearch = url.searchParams.get('category')
+	const providers = await prisma.provider.findMany({
+		select: { id: true, rut: true, name: true, fantasyName: true },
+		orderBy: { name: 'asc' },
+	})
 
-	if (providerSearch) {
-		const providers = await prisma.provider.findMany({
-			select: { id: true, rut: true, name: true, fantasyName: true },
-			where: { rut: { contains: providerSearch } },
-		})
+	const categories = await prisma.family.findMany({
+		select: { id: true, code: true, description: true },
+		orderBy: { code: 'asc' },
+	})
 
-		const categories = null
-		return json({ providers, categories })
-	}
-
-	if (categorySearch) {
-		const categories = await prisma.family.findMany({
-			select: { id: true, code: true, description: true },
-			where: { description: { contains: categorySearch } },
-		})
-		const providers = null
-		return json({ categories, providers })
-	}
-
-	return json({ providers: null, categories: null })
+	return json({ providers, categories })
 }
 
 export { action }
@@ -82,104 +44,111 @@ export default function CreateItem() {
 	const [provider, setProvider] = useState<Provider | null>(null)
 	const [category, setCategory] = useState<Category | null>(null)
 
-	//save provider and category in state, then send it to the server via params
+	const [providerFilter, setProviderFilter] = useState('')
+	const handleProviderFilterChange = (
+		e: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		setProviderFilter(e.target.value)
+	}
+
+	const filteredProviders = providers.filter(provider => {
+		return (
+			provider.name.toLowerCase().includes(providerFilter.toLowerCase()) ||
+			provider.rut.includes(providerFilter)
+		)
+	})
+	const [categoryFilter, setCategoryFilter] = useState('')
+	const handleCategoryFilterChange = (
+		e: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		setCategoryFilter(e.target.value)
+	}
+	const filteredCategories = categories.filter(category => {
+		return (
+			category.description
+				.toLowerCase()
+				.includes(categoryFilter.toLowerCase()) ||
+			category.code.includes(categoryFilter)
+		)
+	})
+
+	const providerAndCategorySelected = provider && category
 
 	return (
-		<div className="max-w-[45rem]">
-			<Stepper
-				canProceed={{
-					1: !!provider,
-					2: !!category,
-				}}
-				title="Registro de nuevo articulo"
-			>
-				<Stepper.Step title="Seleccione un Proveedor">
-					<div className="h-[3rem] text-center text-2xl font-bold">
-						{provider?.name}
+		<div className="flex max-w-[35rem] flex-col  rounded-md bg-secondary">
+			<div className="flex gap-2 rounded-t-md bg-primary/50 p-3 text-2xl">
+				<Icon name="route" />
+				<h1>Ingreso de articulo</h1>
+			</div>
+			<div className="flex flex-col gap-4 p-4">
+				<SelectModal title="Proveedor" selected={provider?.name}>
+					<Input
+						type="text"
+						className="mb-4 w-full"
+						onChange={handleProviderFilterChange}
+						defaultValue={providerFilter}
+					/>
+					<div className="flex max-h-[15rem] flex-col gap-1 overflow-auto">
+						{filteredProviders.map(provider => (
+							<div
+								key={provider.id}
+								className="flex cursor-pointer items-center gap-2 rounded-sm p-1 hover:bg-primary/50"
+								onClick={() => setProvider(provider)}
+							>
+								<span className="w-[7rem]">
+									{provider.rut}-{calculateDV(provider.rut)}
+								</span>{' '}
+								<span className="border-l-2 pl-2">{provider.name}</span>
+							</div>
+						))}
 					</div>
-					<div className="flex w-full flex-col items-center justify-center">
-						<SearchBar
-							autoFocus
-							autoSubmit
-							queryName="provider"
-							status={'idle'}
-							formAction={'/system/inventory/new'}
-							label={'RUT Proveedor'}
-							icon={'magnifying-glass'}
-						/>
-						{providers && (
-							<ul className="my-4 rounded-md bg-background/70 p-4">
-								{providers.length > 0 ? (
-									providers.map(provider => (
-										<li
-											className="flex cursor-pointer rounded-md p-2 hover:bg-primary/40"
-											onClick={() => setProvider(provider)}
-											key={provider.id}
-										>
-											<span className="flex min-w-[8rem] font-bold tracking-wider">
-												{provider.rut}-{calculateDV(provider.rut)}
-											</span>{' '}
-											<span className="border-l-2 pl-4  ">{provider.name}</span>
-										</li>
-									))
-								) : (
-									<h1>Sin coincidencias.</h1>
-								)}
-							</ul>
-						)}
+					<div className="absolute bottom-7 left-7 mt-4  flex w-[20rem] items-center justify-between">
+						<span className="text-xl text-foreground">{provider?.name}</span>
+						<Button
+							onClick={() => setProvider(null)}
+							variant={'ghost'}
+							size={'icon'}
+						>
+							<Icon name="cross-1" />
+						</Button>
 					</div>
-				</Stepper.Step>
-				<Stepper.Step title="Seleccione una Categoría">
-					<div className="h-[3rem] text-center text-2xl font-bold">
-						{category?.code} | {category?.description}
+				</SelectModal>
+				<SelectModal title="Categoría" selected={category?.description}>
+					<Input
+						type="text"
+						className="mb-4 w-full"
+						onChange={handleCategoryFilterChange}
+						defaultValue={categoryFilter}
+					/>
+					<div className="flex max-h-[15rem] flex-col gap-1 overflow-auto">
+						{filteredCategories.map(category => (
+							<div
+								key={category.id}
+								className="flex cursor-pointer items-center gap-2 rounded-sm p-1 hover:bg-primary/50"
+								onClick={() => setCategory(category)}
+							>
+								<span className="w-[7rem]">{category.code}</span>{' '}
+								<span className="border-l-2 pl-2">{category.description}</span>
+							</div>
+						))}
 					</div>
-					<div className="flex w-full flex-col items-center justify-center">
-						<SearchBar
-							autoFocus
-							autoSubmit
-							queryName="category"
-							status={'idle'}
-							formAction={'/system/inventory/new'}
-							label={'Nombre categoría'}
-							icon={'magnifying-glass'}
-						/>
-						{categories && (
-							<ul className="my-4 rounded-md bg-background/70 p-4">
-								{categories.length > 0 ? (
-									categories.map(category => (
-										<li
-											className="flex cursor-pointer rounded-md p-2 hover:bg-primary/40"
-											onClick={() => setCategory(category)}
-											key={category.id}
-										>
-											<span className="flex min-w-[8rem] font-bold tracking-wider">
-												{category.code}
-											</span>{' '}
-											<span className="border-l-2 pl-4  ">
-												{category.description}
-											</span>
-										</li>
-									))
-								) : (
-									<h1>Sin coincidencias.</h1>
-								)}
-							</ul>
-						)}
+					<div className="absolute bottom-7 left-7 mt-4  flex w-[20rem] items-center justify-between">
+						<span className="text-xl text-foreground">
+							{category?.description}
+						</span>
+						<Button
+							onClick={() => setCategory(null)}
+							variant={'ghost'}
+							size={'icon'}
+						>
+							<Icon name="cross-1" />
+						</Button>
 					</div>
-				</Stepper.Step>
-				<Stepper.Step title="Complete la información del articulo">
-					<div className="flex justify-center">
-						<ItemEditor
-							providerId={provider?.id ?? ''}
-							categoryId={category?.id ?? ''}
-						/>
-					</div>
-				</Stepper.Step>
-			</Stepper>
-
-			<SelectModal title='Proveedor'>
-
-			</SelectModal>
+				</SelectModal>
+				{providerAndCategorySelected && (
+					<ItemEditor providerId={provider.id} categoryId={category.id} />
+				)}
+			</div>
 		</div>
 	)
 }
