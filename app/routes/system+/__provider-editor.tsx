7b3bc1d847b0a34e6cc7398fb9aa85e22a4bc28f
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-12-19 23:53:58 -0300

    Refactor code to improve performance and readability

--- app/routes/system+/__provider-editor.tsx
+++ app/routes/system+/__provider-editor.tsx
@@ -1,56 +1,35 @@
-import {
-	conform,
-	list,
-	useFieldList,
-	useFieldset,
-	useForm,
-	type FieldConfig,
-} from '@conform-to/react'
-import { getFieldsetConstraint, parse } from '@conform-to/zod'
-import { createId as cuid } from '@paralleldrive/cuid2'
-import { Provider, type Note, type NoteImage } from '@prisma/client'
-import {
-	unstable_createMemoryUploadHandler as createMemoryUploadHandler,
-	json,
-	unstable_parseMultipartFormData as parseMultipartFormData,
-	redirect,
-	type DataFunctionArgs,
-	type SerializeFrom,
-  ActionFunctionArgs,
-} from '@remix-run/node'
-import { Form, useActionData } from '@remix-run/react'
-import { useRef, useState } from 'react'
-import { AuthenticityTokenInput } from 'remix-utils/csrf/react'
-import { z } from 'zod'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
-import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
-import { ErrorList, Field, TextareaField } from '#app/components/forms.tsx'
+import { ErrorList, Field } from '#app/components/forms.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { Icon } from '#app/components/ui/icon.tsx'
-import { Label } from '#app/components/ui/label.tsx'
 import { StatusButton } from '#app/components/ui/status-button.tsx'
-import { Textarea } from '#app/components/ui/textarea.tsx'
-import { requireUserId } from '#app/utils/auth.server.ts'
 import { validateCSRF } from '#app/utils/csrf.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
-import { cn, getNoteImgSrc, useIsPending } from '#app/utils/misc.tsx'
+import { useIsPending } from '#app/utils/misc.tsx'
+import { conform, useForm } from '@conform-to/react'
+import { getFieldsetConstraint, parse } from '@conform-to/zod'
+import { Provider } from '@prisma/client'
+import {
+	ActionFunctionArgs,
+	json,
+	redirect,
+	type SerializeFrom,
+} from '@remix-run/node'
+import { Form, useActionData } from '@remix-run/react'
+import { AuthenticityTokenInput } from 'remix-utils/csrf/react'
+import { z } from 'zod'
 
-const titleMinLength = 1
-const titleMaxLength = 100
-const contentMinLength = 1
-const contentMaxLength = 10000
+const undefinedDataText = 'Sin datos'
 
 const ProviderEditorSchema = z.object({
-	
 	id: z.string().optional(),
-	rut: z.string(),
-	dv: z.string(),
-	name: z.string(),
-	address: z.string(),
-	city: z.string(),
-	fantasyName: z.string(),
-	phone: z.string(),
-	fax: z.string(),
+	rut: z.string({ required_error: 'Campo obligatorio' }), //TODO: Custom RUT validation
+	name: z.string({ required_error: 'Campo obligatorio' }),
+	address: z.string().optional(),
+	city: z.string().optional(),
+	fantasyName: z.string({ required_error: 'Campo obligatorio' }),
+	phone: z.string().optional(),
+	fax: z.string().optional(),
 })
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -61,8 +40,6 @@ export async function action({ request }: ActionFunctionArgs) {
 
 	const submission = await parse(formData, {
 		schema: ProviderEditorSchema.superRefine(async (data, ctx) => {
-			if (!data.id) return
-
 			const providerByRut = await prisma.provider.findUnique({
 				select: { id: true, rut: true },
 				where: { rut: data.rut },
@@ -76,7 +53,7 @@ export async function action({ request }: ActionFunctionArgs) {
 				})
 			}
 		}),
-    //TODO Need to change so rut has dv in the same field before proceeding
+
 		async: true,
 	})
 
@@ -89,43 +66,43 @@ export async function action({ request }: ActionFunctionArgs) {
 	}
 
 	const {
-		id: itemId,
-		code,
+		id: providerId,
+		rut,
 		name,
-		price,
-		sellingPrice,
-		stock,
-		providerId,
-		categoryId,
+		address,
+		city,
+		fantasyName,
+		phone,
+		fax,
 	} = submission.value
 
-	const updatedItem = await prisma.item.upsert({
+	const updatedProvider = await prisma.provider.upsert({
 		select: { id: true },
-		where: { id: itemId ?? '__new_item__' },
+		where: { id: providerId ?? '__new_provider__' },
 		create: {
-			code,
+			rut,
 			name,
-			price,
-			sellingPrice,
-			stock,
-			provider: { connect: { id: providerId } }, //
-			family: { connect: { id: categoryId } }, //
+			address: address ?? undefinedDataText,
+			city: city ?? undefinedDataText,
+			fantasyName,
+			phone: phone ?? undefinedDataText,
+			fax: fax ?? undefinedDataText,
 		},
 		update: {
-			code,
+			rut,
 			name,
-			price,
-			sellingPrice,
-			stock,
-			provider: { connect: { id: providerId } },
-			family: { connect: { id: categoryId } },
+			address,
+			city,
+			fantasyName,
+			phone,
+			fax,
 		},
 	})
 
-	return redirect(`/system/inventory/${updatedItem.id}`)
+	return redirect(`/system/providers/${updatedProvider.id}`)
 }
 
-export function NoteEditor({
+export function ProviderEditor({
 	provider,
 }: {
 	provider?: SerializeFrom<
@@ -133,16 +110,13 @@ export function NoteEditor({
 			Provider,
 			| 'id'
 			| 'rut'
-			| 'dv'
 			| 'name'
 			| 'address'
 			| 'city'
 			| 'fantasyName'
 			| 'phone'
 			| 'fax'
-		> & {
-			images: Array<Pick<NoteImage, 'id' | 'altText'>>
-		}
+		>
 	>
 }) {
 	const actionData = useActionData<typeof action>()
@@ -150,83 +124,87 @@ export function NoteEditor({
 
 	const [form, fields] = useForm({
 		id: 'provider-editor',
-		constraint: getFieldsetConstraint(NoteEditorSchema),
+		constraint: getFieldsetConstraint(ProviderEditorSchema),
 		lastSubmission: actionData?.submission,
 		onValidate({ formData }) {
-			return parse(formData, { schema: NoteEditorSchema })
+			return parse(formData, { schema: ProviderEditorSchema })
 		},
 		defaultValue: {
-			title: note?.title ?? '',
-			content: note?.content ?? '',
-			images: note?.images ?? [{}],
+			rut: provider?.rut ?? '',
+			name: provider?.name ?? '',
+			address: provider?.address ?? '',
+			city: provider?.city ?? '',
+			fantasyName: provider?.fantasyName ?? '',
+			phone: provider?.phone ?? '',
+			fax: provider?.fax ?? '',
 		},
 	})
-	const imageList = useFieldList(form.ref, fields.images)
 
 	return (
-		<div className="absolute inset-0">
-			<Form
-				method="POST"
-				className="flex h-full flex-col gap-y-4 overflow-y-auto overflow-x-hidden px-10 pb-28 pt-12"
-				{...form.props}
-				encType="multipart/form-data"
-			>
+		<div className="flex flex-col gap-4 p-6">
+			<Form method="POST" className="flex  flex-col gap-y-3" {...form.props}>
 				<AuthenticityTokenInput />
 
-				{note ? <input type="hidden" name="id" value={note.id} /> : null}
+				{provider ? (
+					<input type="hidden" name="id" value={provider.id} />
+				) : null}
 				<div className="flex flex-col gap-1">
 					<Field
-						labelProps={{ children: 'Title' }}
+						labelProps={{ children: 'RUT' }}
 						inputProps={{
 							autoFocus: true,
-							...conform.input(fields.title, { ariaAttributes: true }),
+							...conform.input(fields.rut, { ariaAttributes: true }),
 						}}
-						errors={fields.title.errors}
+						errors={fields.rut.errors}
 					/>
-					<TextareaField
-						labelProps={{ children: 'Content' }}
-						textareaProps={{
-							...conform.textarea(fields.content, { ariaAttributes: true }),
+					<Field
+						labelProps={{ children: 'Nombre' }}
+						inputProps={{
+							...conform.input(fields.name, { ariaAttributes: true }),
 						}}
-						errors={fields.content.errors}
+						errors={fields.name.errors}
 					/>
-					<div>
-						<Label>Images</Label>
-						<ul className="flex flex-col gap-4">
-							{imageList.map((image, index) => (
-								<li
-									key={image.key}
-									className="relative border-b-2 border-muted-foreground"
-								>
-									<button
-										className="absolute right-0 top-0 text-foreground-destructive"
-										{...list.remove(fields.images.name, { index })}
-									>
-										<span aria-hidden>
-											<Icon name="cross-1" />
-										</span>{' '}
-										<span className="sr-only">Remove image {index + 1}</span>
-									</button>
-									<ImageChooser config={image} />
-								</li>
-							))}
-						</ul>
-					</div>
-					<Button
-						className="mt-3"
-						{...list.insert(fields.images.name, { defaultValue: {} })}
-					>
-						<span aria-hidden>
-							<Icon name="plus">Image</Icon>
-						</span>{' '}
-						<span className="sr-only">Add image</span>
-					</Button>
+					<Field
+						labelProps={{ children: 'Dirección' }}
+						inputProps={{
+							...conform.input(fields.address, { ariaAttributes: true }),
+						}}
+						errors={fields.address.errors}
+					/>
+					<Field
+						labelProps={{ children: 'Ciudad' }}
+						inputProps={{
+							...conform.input(fields.city, { ariaAttributes: true }),
+						}}
+						errors={fields.city.errors}
+					/>
+					<Field
+						labelProps={{ children: 'Nombre de Empresa' }}
+						inputProps={{
+							...conform.input(fields.fantasyName, { ariaAttributes: true }),
+						}}
+						errors={fields.fantasyName.errors}
+					/>
+					<Field
+						labelProps={{ children: 'Teléfono' }}
+						inputProps={{
+							...conform.input(fields.phone, { ariaAttributes: true }),
+						}}
+						errors={fields.phone.errors}
+					/>
+					<Field
+						labelProps={{ children: 'Fax' }}
+						inputProps={{
+							...conform.input(fields.fax, { ariaAttributes: true }),
+						}}
+						errors={fields.fax.errors}
+					/>
 				</div>
 				<ErrorList id={form.errorId} errors={form.errors} />
 			</Form>
-			<div className={floatingToolbarClassName}>
-				<Button form={form.id} variant="destructive" type="reset">
-					Reset
+			<div className=" flex justify-between">
+				<Button form={form.id} variant="ghost" type="reset">
+					Restaurar
 				</Button>
 				<StatusButton
 					form={form.id}
@@ -234,115 +212,11 @@ export function NoteEditor({
 					disabled={isPending}
 					status={isPending ? 'pending' : 'idle'}
 				>
-					Submit
+					<Icon name="check" className="mr-2" />{' '}
+					{provider ? 'Actualizar' : 'Ingresar Proveedor'}
 				</StatusButton>
 			</div>
 		</div>
-	)
-}
-
-function ImageChooser({
-	config,
-}: {
-	config: FieldConfig<z.infer<typeof ImageFieldsetSchema>>
-}) {
-	const ref = useRef<HTMLFieldSetElement>(null)
-	const fields = useFieldset(ref, config)
-	const existingImage = Boolean(fields.id.defaultValue)
-	const [previewImage, setPreviewImage] = useState<string | null>(
-		fields.id.defaultValue ? getNoteImgSrc(fields.id.defaultValue) : null,
-	)
-	const [altText, setAltText] = useState(fields.altText.defaultValue ?? '')
-
-	return (
-		<fieldset
-			ref={ref}
-			aria-invalid={Boolean(config.errors?.length) || undefined}
-			aria-describedby={config.errors?.length ? config.errorId : undefined}
-		>
-			<div className="flex gap-3">
-				<div className="w-32">
-					<div className="relative h-32 w-32">
-						<label
-							htmlFor={fields.file.id}
-							className={cn('group absolute h-32 w-32 rounded-lg', {
-								'bg-accent opacity-40 focus-within:opacity-100 hover:opacity-100':
-									!previewImage,
-								'cursor-pointer focus-within:ring-4': !existingImage,
-							})}
-						>
-							{previewImage ? (
-								<div className="relative">
-									<img
-										src={previewImage}
-										alt={altText ?? ''}
-										className="h-32 w-32 rounded-lg object-cover"
-									/>
-									{existingImage ? null : (
-										<div className="pointer-events-none absolute -right-0.5 -top-0.5 rotate-12 rounded-sm bg-secondary px-2 py-1 text-xs text-secondary-foreground shadow-md">
-											new
-										</div>
-									)}
-								</div>
-							) : (
-								<div className="flex h-32 w-32 items-center justify-center rounded-lg border border-muted-foreground text-4xl text-muted-foreground">
-									<Icon name="plus" />
-								</div>
-							)}
-							{existingImage ? (
-								<input
-									{...conform.input(fields.id, {
-										type: 'hidden',
-										ariaAttributes: true,
-									})}
-								/>
-							) : null}
-							<input
-								aria-label="Image"
-								className="absolute left-0 top-0 z-0 h-32 w-32 cursor-pointer opacity-0"
-								onChange={event => {
-									const file = event.target.files?.[0]
-
-									if (file) {
-										const reader = new FileReader()
-										reader.onloadend = () => {
-											setPreviewImage(reader.result as string)
-										}
-										reader.readAsDataURL(file)
-									} else {
-										setPreviewImage(null)
-									}
-								}}
-								accept="image/*"
-								{...conform.input(fields.file, {
-									type: 'file',
-									ariaAttributes: true,
-								})}
-							/>
-						</label>
-					</div>
-					<div className="min-h-[32px] px-4 pb-3 pt-1">
-						<ErrorList id={fields.file.errorId} errors={fields.file.errors} />
-					</div>
-				</div>
-				<div className="flex-1">
-					<Label htmlFor={fields.altText.id}>Alt Text</Label>
-					<Textarea
-						onChange={e => setAltText(e.currentTarget.value)}
-						{...conform.textarea(fields.altText, { ariaAttributes: true })}
-					/>
-					<div className="min-h-[32px] px-4 pb-3 pt-1">
-						<ErrorList
-							id={fields.altText.errorId}
-							errors={fields.altText.errors}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className="min-h-[32px] px-4 pb-3 pt-1">
-				<ErrorList id={config.errorId} errors={config.errors} />
-			</div>
-		</fieldset>
 	)
 }
 
