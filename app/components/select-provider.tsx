ab33b9e1d9366c1b72a04a1da43ad482acd05215
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-12-21 15:51:55 -0300

    Add SelectProvider component

---
+++ app/components/select-provider.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { Input } from './ui/input.tsx'
+import { SelectModal } from './ui/select-modal.tsx'
+
+export type Provider = {
+	id: string
+	rut: string
+	name: string
+	fantasyName: string
+}
+
+export function SelectProvider({
+	providers,
+	selectedProvider,
+	setSelectedProvider,
+}: {
+	providers: Provider[]
+	selectedProvider: Provider | null
+	setSelectedProvider: React.Dispatch<React.SetStateAction<Provider | null>>
+}) {
+	const [providerModalOpen, setProviderModalOpen] = useState(false)
+	const [providerFilter, setProviderFilter] = useState('')
+	const handleProviderFilterChange = (
+		e: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		setProviderFilter(e.target.value)
+	}
+	const filteredProviders = providers.filter(provider => {
+		return (
+			provider.name.toLowerCase().includes(providerFilter.toLowerCase()) ||
+			provider.rut.includes(providerFilter)
+		)
+	})
+
+	return (
+		<SelectModal
+			open={providerModalOpen}
+			onOpenChange={setProviderModalOpen}
+			title="Proveedor"
+			selected={selectedProvider?.name}
+		>
+			<Input
+				autoFocus
+				type="text"
+				className="mb-4 w-full"
+				onChange={handleProviderFilterChange}
+				defaultValue={providerFilter}
+			/>
+			<div className="flex max-h-[15rem] flex-col gap-1 overflow-auto">
+				{filteredProviders.map(provider => (
+					<div
+						key={provider.id}
+						className="flex cursor-pointer items-center gap-2 rounded-sm p-1 hover:bg-primary/50"
+						onClick={() => {
+							setSelectedProvider(provider)
+							setProviderModalOpen(false)
+						}}
+					>
+						<span className="w-[7rem]">{provider.rut}</span>{' '}
+						<span className="border-l-2 pl-2">{provider.name}</span>
+					</div>
+				))}
+			</div>
+		</SelectModal>
+	)
+}
