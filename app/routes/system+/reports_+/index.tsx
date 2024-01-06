b68689c618add863cdc223727bb06494b6e89b90
Cristobal Pulgar <cristobal.npe@gmail.com>
2024-01-06 18:09:55 -0300

    Refactor transaction reports page and add date filters

--- app/routes/system+/reports_+/index.tsx
+++ app/routes/system+/reports_+/index.tsx
@@ -6,20 +6,52 @@ import {
 	TableHeader,
 	TableRow,
 } from '#app/components/ui/table.tsx'
+import { requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { cn, formatCurrency } from '#app/utils/misc.tsx'
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData, useNavigate } from '@remix-run/react'
-import { formatRelative } from 'date-fns'
+import {
+	NavLink,
+	useLoaderData,
+	useLocation,
+	useNavigate,
+} from '@remix-run/react'
+import { format } from 'date-fns'
 import {
 	TRANSACTION_STATUS_COMPLETED,
 	TRANSACTION_STATUS_DISCARDED,
 	TRANSACTION_STATUS_PENDING,
 } from '../sell.tsx'
-import { requireUserId } from '#app/utils/auth.server.ts'
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request)
+
+	const url = new URL(request.url)
+	let since = url.searchParams.get('since')
+
+	if (!since) {
+		since = 'today'
+	}
+
+	// map since to date
+	let sinceDate = new Date()
+	switch (since) {
+		case 'today':
+			sinceDate = new Date(new Date().setDate(new Date().getDate() - 1))
+			break
+		case 'last-week':
+			sinceDate = new Date(new Date().setDate(new Date().getDate() - 7))
+			break
+		case 'last-month':
+			sinceDate = new Date(new Date().setMonth(new Date().getMonth() - 1))
+			break
+		case 'all':
+			sinceDate = new Date(0)
+			break
+		default:
+			sinceDate = new Date(new Date().setDate(new Date().getDate() - 1))
+			break
+	}
 
 	//If user is admin, gets all, otherwise only gets their own
 
@@ -31,7 +63,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
 			total: true,
 			seller: { select: { name: true } },
 		},
-		// where: { sellerId: userId },
+		where: { createdAt: { gte: sinceDate } },
+		//first order by completedAt, then by createdAt
+		// orderBy: { completedAt: 'desc', createdAt: 'desc' },
 		orderBy: { createdAt: 'desc' },
 	})
 
@@ -46,8 +80,30 @@ export default function ReportsRoute() {
 	// Seller should only see their own transactions
 	// Admin should see all transactions
 
+	const filters = [
+		{
+			label: 'Ultimas 24 hrs.',
+			query: 'today',
+		},
+		{
+			label: 'Ultima Semana',
+			query: 'last-week',
+		},
+		{
+			label: 'Ultimo Mes',
+			query: 'last-month',
+		},
+		{
+			label: 'Todas',
+			query: 'all',
+		},
+	]
+
+	const { search } = useLocation()
+	const since = new URLSearchParams(search).get('since')
+
 	return (
-		<>
+		<div className='max-w-[90rem]'>
 			<div className="flex justify-between border-b-2 border-secondary pb-4">
 				<div className="flex items-center gap-4">
 					<h1 className="text-2xl">Reportes de Transacciones</h1>
@@ -57,16 +113,30 @@ export default function ReportsRoute() {
 				</div>
 				<div className="flex items-center gap-6">
 					{/* possible buttons here */}
+					{filters.map(filter => (
+						<NavLink
+							key={filter.query}
+							className={({ isActive }) =>
+								cn(
+									'rounded-md px-2 py-1 text-foreground/70 hover:text-foreground/100 ',
+									isActive && since === filter.query && 'bg-primary/70 text-foreground ',
+								)
+							}
+							to={`?since=${filter.query}`}
+						>
+							{filter.label}
+						</NavLink>
+					))}
 				</div>
 			</div>
 			<Table>
 				<TableHeader className="bg-secondary ">
 					<TableRow>
-						<TableHead>ID</TableHead>
-						<TableHead>Creado en</TableHead>
+						<TableHead className="w-[20rem]">ID</TableHead>
+						<TableHead className="w-[13rem]">Fecha Inicio</TableHead>
 						{isAdmin && <TableHead>Vendedor</TableHead>}
-						<TableHead>Total</TableHead>
-						<TableHead>Estado</TableHead>
+						<TableHead className="w-[10rem]">Total</TableHead>
+						<TableHead className="w-[12rem]">Estado</TableHead>
 					</TableRow>
 				</TableHeader>
 				<TableBody>
@@ -79,14 +149,21 @@ export default function ReportsRoute() {
 								{transaction.id}
 							</TableCell>
 							<TableCell>
-								{formatRelative(new Date(transaction.createdAt), new Date())}
+								{format(
+									new Date(transaction.createdAt),
+									"dd/MM/yyyy HH:mm' hrs.'",
+								)}
 							</TableCell>
 							{isAdmin && (
 								<TableCell>
 									{transaction.seller ? transaction.seller.name : 'Desconocido'}
 								</TableCell>
 							)}
-							<TableCell>{formatCurrency(transaction.total)}</TableCell>
+							<TableCell className="font-bold">
+								{transaction.status === TRANSACTION_STATUS_COMPLETED
+									? formatCurrency(transaction.total)
+									: null}
+							</TableCell>
 							<TableCell
 								className={cn(
 									'',
@@ -104,6 +181,6 @@ export default function ReportsRoute() {
 					))}
 				</TableBody>
 			</Table>
-		</>
+		</div>
 	)
 }
