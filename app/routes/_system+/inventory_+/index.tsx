7480cbacabe7d7f84443973b973b7784df4f923e
Cristobal Pulgar <cristobal.npe@gmail.com>
2024-03-25 11:34:25 -0300

    fix prisma queries//add loader

--- app/routes/_system+/inventory_+/index.tsx
+++ app/routes/_system+/inventory_+/index.tsx
@@ -1,5 +1,6 @@
 import { Spacer } from '#app/components/spacer.tsx'
 import { Badge } from '#app/components/ui/badge.tsx'
+import { Button } from '#app/components/ui/button.tsx'
 import {
 	Card,
 	CardContent,
@@ -8,6 +9,8 @@ import {
 	CardTitle,
 } from '#app/components/ui/card.tsx'
 import { Icon, IconName } from '#app/components/ui/icon.tsx'
+import { Input } from '#app/components/ui/input.tsx'
+import { StatusButton } from '#app/components/ui/status-button.tsx'
 import {
 	Table,
 	TableBody,
@@ -18,79 +21,141 @@ import {
 } from '#app/components/ui/table.tsx'
 import { requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
-import { formatCurrency } from '#app/utils/misc.tsx'
-import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData, useNavigate } from '@remix-run/react'
+import { formatCurrency, useDebounce, useIsPending } from '#app/utils/misc.tsx'
+import { Label } from '@radix-ui/react-label'
+import { defer, type LoaderFunctionArgs } from '@remix-run/node'
+import {
+	Await,
+	Form,
+	Link,
+	useLoaderData,
+	useNavigate,
+	useSearchParams,
+	useSubmit,
+} from '@remix-run/react'
+import { Suspense, useId } from 'react'
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	await requireUserId(request)
-	// const url = new URL(request.url)
-	// const searchTerm = url.searchParams.get('search')
+	const url = new URL(request.url)
+	const searchTerm = url.searchParams.get('search') ?? ''
 
-	// const byProvider = url.searchParams.get('by-provider')
+	const searchTermIsCode = !isNaN(parseInt(searchTerm))
 
-	// if (byProvider) {
-	// 	const items = prisma.item
-	// 		.findMany({
-	// 			orderBy: { code: 'asc' },
-	// 			select: {
-	// 				id: true,
-	// 				code: true,
-	// 				name: true,
-	// 				sellingPrice: true,
-	// 				price: true,
-	// 				stock: true,
-	// 				family: { select: { description: true } },
-	// 			},
-	// 			where: {
-	// 				providerId: byProvider,
-	// 			},
-	// 		})
-	// 		.then(u => u)
+	let itemsPromise
 
-	// 	const totalItems = await prisma.item.count()
+	if (searchTerm === '') {
+		itemsPromise = prisma.item
+			.findMany({
+				orderBy: { code: 'asc' },
+				select: {
+					id: true,
+					code: true,
+					name: true,
+					sellingPrice: true,
+					stock: true,
+					family: { select: { description: true } },
+				},
+			})
+			.then(u => u)
+	}
+	if (searchTermIsCode) {
+		itemsPromise = prisma.item
+			.findMany({
+				orderBy: { code: 'asc' },
+				select: {
+					id: true,
+					code: true,
+					name: true,
+					sellingPrice: true,
+					stock: true,
+					family: { select: { description: true } },
+				},
+				where: {
+					code: parseInt(searchTerm),
+				},
+			})
+			.then(u => u)
+	} else {
+		itemsPromise = prisma.item
+			.findMany({
+				orderBy: { code: 'asc' },
+				select: {
+					id: true,
+					code: true,
+					name: true,
+					sellingPrice: true,
+					stock: true,
+					family: { select: { description: true } },
+				},
+				where: {
+					name: { contains: searchTerm },
+				},
+			})
+			.then(u => u)
+	}
 
-	// 	return defer({ items, totalItems })
-	// }
+	const totalItemsPromise = prisma.item.count()
 
-	// if (searchTerm === '') {
-	// 	return redirect('/inventory')
-	// }
-
-	const items = await prisma.item.findMany({
-		orderBy: { code: 'asc' },
-		select: {
-			id: true,
-			code: true,
-			name: true,
-			sellingPrice: true,
-			stock: true,
-			family: { select: { description: true } },
-		},
-		// where: {
-		// 	code: searchTerm ? parseInt(searchTerm.toString()) : undefined,
-		// },
-	})
-
-	const totalItems = await prisma.item.count()
-
-	const noStockItems = await prisma.item.count({
+	const noStockItemsPromise = prisma.item.count({
 		where: {
 			stock: 0,
 		},
 	})
 
-	return json({ items, totalItems, noStockItems })
+	const lowStockItemsPromise = prisma.item.count({
+		where: {
+			stock: { lte: 5, gt: 0 },
+		},
+	})
+
+	const newItemsThisMonthPromise = prisma.item.count({
+		where: {
+			createdAt: {
+				gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+			},
+		},
+	})
+
+	const [totalItems, noStockItems, lowStockItems, newItemsThisMonth] =
+		await Promise.all([
+			totalItemsPromise,
+			noStockItemsPromise,
+			lowStockItemsPromise,
+			newItemsThisMonthPromise,
+		])
+
+	return defer({
+		itemsPromise,
+		totalItems,
+		noStockItems,
+		lowStockItems,
+		newItemsThisMonth,
+	})
 }
 
 export default function InventoryRoute() {
 	const isAdmin = true
-	const { items, totalItems, noStockItems } = useLoaderData<typeof loader>()
+	const {
+		itemsPromise,
+		totalItems,
+		noStockItems,
+		lowStockItems,
+		newItemsThisMonth,
+	} = useLoaderData<typeof loader>()
 
 	return (
 		<main className="flex flex-col">
-			<div className="flex justify-between border-b-2 border-secondary pb-3">
+			<div className="flex flex-col items-center justify-between gap-2 border-b-2 border-secondary pb-3 text-center md:flex-row md:text-left">
 				<h1 className="text-xl font-semibold">Administración de Inventario</h1>
+				{isAdmin && (
+					<Button asChild variant={'outline'}>
+						<Link className="flex items-center justify-center gap-2" to="new">
+							<Icon name="plus" size="md" />
+							<span>Agregar articulo</span>
+						</Link>
+					</Button>
+				)}
 			</div>
 			<Spacer size={'4xs'} />
 			<div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
@@ -98,13 +163,15 @@ export default function InventoryRoute() {
 					title={'Artículos Registrados'}
 					value={`${totalItems}`}
 					icon={'package'}
-					subtext={'5 artículos nuevos este mes'}
+					subtext={`${newItemsThisMonth} ${
+						newItemsThisMonth === 1 ? 'articulo' : 'artículos'
+					} nuevos este mes`}
 				/>
 				<DataCard
 					title={'Artículos Sin Stock'}
 					value={`${noStockItems}`}
 					icon={'box-off'}
-					subtext={'15 artículos próximos a agotarse'}
+					subtext={`${lowStockItems} artículos próximos a agotarse`}
 				/>
 				<DataCard
 					title={'Mayores Ingresos'}
@@ -121,7 +188,15 @@ export default function InventoryRoute() {
 			</div>
 			<Spacer size={'4xs'} />
 
-			<ItemsTableCard items={items} />
+			<Suspense
+				fallback={
+					<LoadingSpinner description={`Cargando ${totalItems} artículos...`} />
+				}
+			>
+				<Await resolve={itemsPromise}>
+					{items => <ItemsTableCard items={items} />}
+				</Await>
+			</Suspense>
 		</main>
 	)
 }
@@ -165,21 +240,22 @@ function ItemsTableCard({ items }: { items: ItemData[] }) {
 	const navigate = useNavigate()
 
 	return (
-		<Card className=" max-h-[63dvh] overflow-y-scroll xl:col-span-2">
-			<CardHeader className="flex flex-row items-center ">
+		<Card className=" max-h-[63dvh]  xl:col-span-2">
+			<CardHeader className="flex flex-col items-center md:flex-row md:justify-between">
 				<div className="grid gap-2">
 					<CardTitle>Artículos</CardTitle>
 					<CardDescription>
 						Lista de artículos registrados en sistema.
 					</CardDescription>
 				</div>
-				{/* Search bar goes here */}
+
+				<InventorySearchBar status="idle" autoSubmit />
 			</CardHeader>
-			<CardContent>
+			<CardContent className="h-[45dvh] overflow-y-auto">
 				<Table>
 					<TableHeader>
 						<TableRow>
-							<TableHead>Código / Descripción</TableHead>
+							<TableHead>Descripción / Código</TableHead>
 							<TableHead className="hidden lg:table-cell">Categoría</TableHead>
 							<TableHead className="hidden md:table-cell">Stock</TableHead>
 							<TableHead className="text-right">Precio Venta</TableHead>
@@ -195,7 +271,7 @@ function ItemsTableCard({ items }: { items: ItemData[] }) {
 								<TableCell>
 									<div className="font-medium uppercase">{item.name}</div>
 									<div className="text-xs uppercase text-muted-foreground">
-										{item.id}
+										{item.code}
 									</div>
 								</TableCell>
 								<TableCell className="hidden lg:table-cell">
@@ -215,5 +291,76 @@ function ItemsTableCard({ items }: { items: ItemData[] }) {
 				</Table>
 			</CardContent>
 		</Card>
+	)
+}
+
+function InventorySearchBar({
+	status,
+	autoFocus = false,
+	autoSubmit = false,
+}: {
+	status: 'idle' | 'pending' | 'success' | 'error'
+
+	autoFocus?: boolean
+	autoSubmit?: boolean
+}) {
+	const id = useId()
+	const [searchParams] = useSearchParams()
+	const submit = useSubmit()
+	const isSubmitting = useIsPending({
+		formMethod: 'GET',
+		formAction: '/inventory',
+	})
+
+	const handleFormChange = useDebounce((form: HTMLFormElement) => {
+		submit(form)
+	}, 400)
+
+	return (
+		<Form
+			method="GET"
+			action="/inventory"
+			className="flex flex-wrap items-center justify-center gap-1"
+			onChange={e => autoSubmit && handleFormChange(e.currentTarget)}
+		>
+			<div className="flex-1">
+				<Label htmlFor={id} className="sr-only">
+					Buscar
+				</Label>
+				<Input
+					type="text"
+					name="search"
+					id={id}
+					defaultValue={searchParams.get('search') ?? ''}
+					placeholder="Búsqueda"
+					className=" [&::-webkit-inner-spin-button]:appearance-none"
+					autoFocus={autoFocus}
+				/>
+			</div>
+			<div>
+				<StatusButton
+					type="submit"
+					status={isSubmitting ? 'pending' : status}
+					className="flex w-full items-center justify-center"
+					size="sm"
+				>
+					<Icon name="magnifying-glass" size="md" />
+					<span className="sr-only">Buscar</span>
+				</StatusButton>
+			</div>
+		</Form>
+	)
+}
+
+function LoadingSpinner({
+	description = 'Cargando...',
+}: {
+	description?: string
+}) {
+	return (
+		<div className="flex h-full w-full flex-col items-center justify-center gap-3 p-16  text-2xl">
+			<Icon name="update" className="animate-spin text-4xl" />
+			<div className="text-muted-foreground">{description}</div>
+		</div>
 	)
 }
