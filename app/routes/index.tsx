9d0d6e2d1c070ad138f65353d99057b3a2ed523c
Cristobal Pulgar <cristobal.npe@gmail.com>
2024-04-03 14:53:38 -0300

    change title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -22,11 +22,9 @@ export default function Index() {
 		<main className=" flex h-full items-center justify-center ">
 			<div className="text-center">
 				<h1 className="mb-4 text-3xl font-bold text-foreground/70 sm:text-4xl">
-					Sistema de Ventas
+				Sistema de Ventas
 				</h1>
-				<h1 className="text-4xl font-bold sm:text-6xl">
-					Selim Amar y CIA LTDA.
-				</h1>
+				<h1 className="text-4xl font-bold sm:text-6xl">Ventesca</h1>
 				<Button asChild className="mt-20 text-xl sm:px-20 sm:py-8" size={'lg'}>
 					<Link to={'login'}>Ingresar</Link>
 				</Button>
