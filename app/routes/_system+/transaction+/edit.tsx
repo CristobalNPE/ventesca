259e1fc8e9e9e55c871b46bacee81c3d8b2c1b30
Cristobal Pulgar <cristobal.npe@gmail.com>
2024-05-01 06:56:13 -0400

    add resource route for changing type and quantity of item transaction

--- app/routes/_system+/transaction+/edit.tsx
+++ app/routes/_system+/transaction+/edit.tsx
@@ -1,10 +1,19 @@
 import { requireUserId } from '#app/utils/auth.server.ts'
-import { ActionFunctionArgs, json } from '@remix-run/node'
+import { prisma } from '#app/utils/db.server.ts'
+import { ActionFunctionArgs, json, redirect } from '@remix-run/node'
+import {
+	UPDATE_IT_QUANTITY,
+	UpdateItemTransactionQuantitySchema,
+} from './_components/itemTransaction-quantitySelector.tsx'
 import {
 	UPDATE_IT_TYPE,
 	UpdateItemTransactionTypeSchema,
 } from './_components/itemTransaction-typeToggle.tsx'
-import { prisma } from '#app/utils/db.server.ts'
+import { TYPE_RETURN } from './_types/item-transactionType.ts'
+
+export async function loader() {
+	return redirect('/transaction')
+}
 
 export async function action({ request }: ActionFunctionArgs) {
 	await requireUserId(request)
@@ -26,14 +35,63 @@ export async function action({ request }: ActionFunctionArgs) {
 
 			const { itemTransactionId, itemTransactionType } = result.data
 
+			let { totalPrice } = await prisma.itemTransaction.findUniqueOrThrow({
+				where: { id: itemTransactionId },
+				select: { totalPrice: true },
+			})
+
+			if (itemTransactionType === TYPE_RETURN) {
+				totalPrice = Math.abs(totalPrice) * -1
+			} else {
+				totalPrice = Math.abs(totalPrice)
+			}
+
 			await prisma.itemTransaction.update({
 				where: { id: itemTransactionId },
-				data: { type: itemTransactionType },
+				data: { type: itemTransactionType, totalPrice },
 			})
 
 			return json({ status: 'ok' } as const, { status: 200 })
 		}
 		default:
 			return json({ status: 'error' } as const, { status: 400 })
+
+		case UPDATE_IT_QUANTITY: {
+			const result = UpdateItemTransactionQuantitySchema.safeParse({
+				itemTransactionId: formData.get('itemTransactionId'),
+				itemTransactionQuantity: Number(
+					formData.get('itemTransactionQuantity'),
+				),
+			})
+
+			if (!result.success) {
+				const errors = result.error.flatten()
+				return json({ status: 'error', errors } as const, { status: 400 })
+			}
+
+			const { itemTransactionId, itemTransactionQuantity } = result.data
+
+			const {
+				type,
+				item: { sellingPrice },
+			} = await prisma.itemTransaction.findUniqueOrThrow({
+				where: { id: itemTransactionId },
+				select: { type: true, item: { select: { sellingPrice: true } } },
+			})
+
+			let totalPrice = sellingPrice * itemTransactionQuantity
+
+			if (type === TYPE_RETURN) {
+				totalPrice = Math.abs(totalPrice) * -1
+			} else {
+				totalPrice = Math.abs(totalPrice)
+			}
+
+			await prisma.itemTransaction.update({
+				where: { id: itemTransactionId },
+				data: { quantity: itemTransactionQuantity, totalPrice: totalPrice },
+			})
+			return json({ status: 'ok' } as const, { status: 200 })
+		}
 	}
 }
