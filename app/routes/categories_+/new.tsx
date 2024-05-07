513b26ee6cb86cc47e08c6bc952d2681aa318e3b
Cristobal Pulgar <cristobal.npe@gmail.com>
2024-05-07 18:52:23 -0400

    comment out old code to avoid errors

--- app/routes/categories_+/new.tsx
+++ app/routes/categories_+/new.tsx
@@ -1,14 +1,14 @@
-import { Icon } from '#app/components/ui/icon.tsx'
-import { CategoryEditor, action } from './__category-editor.tsx'
-export { action }
-export default function CreateCategory() {
-	return (
-		<div className="flex max-w-[35rem] flex-col  rounded-md bg-secondary">
-			<div className="flex gap-4 rounded-t-md bg-primary p-3 text-2xl text-background">
-				<Icon name="route" />
-				<h1>Ingresar nueva categoría</h1>
-			</div>
-			<CategoryEditor />
-		</div>
-	)
-}
+// import { Icon } from '#app/components/ui/icon.tsx'
+// import { CategoryEditor, action } from './__category-editor.tsx'
+// export { action }
+// export default function CreateCategory() {
+// 	return (
+// 		<div className="flex max-w-[35rem] flex-col  rounded-md bg-secondary">
+// 			<div className="flex gap-4 rounded-t-md bg-primary p-3 text-2xl text-background">
+// 				<Icon name="route" />
+// 				<h1>Ingresar nueva categoría</h1>
+// 			</div>
+// 			<CategoryEditor />
+// 		</div>
+// 	)
+// }
