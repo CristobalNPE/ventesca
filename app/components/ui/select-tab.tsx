3287e19174103197b441c7bff02131b0c10ded80
Cristobal Pulgar <cristobal.npe@gmail.com>
2024-05-07 18:50:26 -0400

    refactor to use conform data instead of controlled state

--- app/components/ui/select-tab.tsx
+++ app/components/ui/select-tab.tsx
@@ -10,36 +10,35 @@ export const SelectTab = ({
 	label,
 	options,
 	name,
-	selected,
-	setSelected,
+	initialValue,
 }: {
 	label?: string
 	options: SelectTabOption[]
 	name: string
-	selected: string
-	setSelected: (value: any) => void
+	initialValue?: string
 }) => {
 	return (
 		<div className="flex flex-col gap-1">
-			{label && <div className=" text-body-xs text-muted-foreground">{label}</div>}
-			<div className="flex w-full select-none  justify-between rounded-md bg-background p-1 text-sm shadow-sm border">
+			{label && (
+				<div className=" text-body-xs text-muted-foreground">{label}</div>
+			)}
+			<div className="flex w-full select-none  justify-between rounded-md border bg-background p-1 text-sm shadow-sm">
 				{options.map((option, index) => (
 					<label
 						key={option.value}
-						className=" flex w-[50%] cursor-pointer items-center justify-center gap-2 rounded-md bg-background p-2 text-center capitalize  transition-colors duration-200 has-[:checked]:border border-primary has-[:checked]:bg-card has-[:checked]:font-bold"
+						className=" flex w-[50%] cursor-pointer items-center justify-center gap-2 rounded-md border-primary bg-background p-2 text-center  capitalize transition-colors duration-200 has-[:checked]:border has-[:checked]:bg-card has-[:checked]:font-bold"
 						htmlFor={option.value}
 					>
 						<input
-							defaultChecked={selected === option.value }
 							className="appearance-none"
 							type="radio"
 							name={name}
-							value={selected}
-							onChange={() => setSelected(option.value)}
+							value={option.value}
 							id={option.value}
+							defaultChecked={initialValue === option.value}
 						/>
 						<span>{option.label}</span>
-						{option.icon && <Icon name={option.icon} className='text-xl' />}
+						{option.icon && <Icon name={option.icon} className="text-xl" />}
 					</label>
 				))}
 			</div>
