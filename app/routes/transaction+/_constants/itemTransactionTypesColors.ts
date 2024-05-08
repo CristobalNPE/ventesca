3177f611c2b9ad05f9a397f71a70445a6ad9bc30
Cristobal Pulgar <cristobal.npe@gmail.com>
2024-05-08 07:13:38 -0400

    change color to not depend on theme

--- app/routes/transaction+/_constants/itemTransactionTypesColors.ts
+++ app/routes/transaction+/_constants/itemTransactionTypesColors.ts
@@ -6,7 +6,7 @@ import {
 } from '../_types/item-transactionType.ts'
 
 export const itemTransactionTypeColors: Record<ItemTransactionType, string> = {
-	[TYPE_SELL]: 'bg-primary/80',
+	[TYPE_SELL]: 'bg-green-500/80',
 	[TYPE_PROMO]: 'bg-blue-500/80',
 	[TYPE_RETURN]: 'bg-orange-500/80',
 }
