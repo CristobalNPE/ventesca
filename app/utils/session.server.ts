2aefc5fe66b858fc6e1f299484a7f09d16bfa36b
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-06-09 18:05:11 -0400

    change cookie name

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -2,7 +2,7 @@ import { createCookieSessionStorage } from '@remix-run/node'
 
 export const authSessionStorage = createCookieSessionStorage({
 	cookie: {
-		name: 'en_session',
+		name: 'ventesca_session',
 		sameSite: 'lax', // CSRF protection is advised if changing to 'none'
 		path: '/',
 		httpOnly: true,
