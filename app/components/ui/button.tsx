1a2300296ca0a23fb1e9ebf4b19bd9f77b2d32ad
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-06-09 17:28:20 -0400

    add sizes

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -20,10 +20,12 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+  			default: 'h-10 px-4 py-2',
+				wide: 'px-24 py-5',
+				sm: 'h-9 rounded-md px-3',
+				lg: 'h-11 rounded-md px-8',
+				pill: 'px-12 py-3 leading-3',
+				icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
