c0160b1b1ab6491b2d83aad39b155ea77ba2859c
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-06-09 17:32:07 -0400

    add role requirement to loader

--- app/routes/_suppliers+/suppliers.new.tsx
+++ app/routes/_suppliers+/suppliers.new.tsx
@@ -1,8 +1,15 @@
 import { SupplierEditor } from './__supplier-editor.tsx'
 import {action} from './__supplier-editor.server.tsx'
+import { requireUserWithRole } from '#app/utils/permissions.server.ts'
+import { LoaderFunctionArgs } from '@remix-run/node'
 
 export { action }
+export async function loader({request}:LoaderFunctionArgs){
+	await requireUserWithRole(request, "Administrador")
 
-export default function CreateItem() {
+	return null;
+}
+
+export default function CreateSupplier() {
 	return <SupplierEditor />
 }
