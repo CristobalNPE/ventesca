57464835b0e881e5d43ca208f604bed181bd8a0f
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-06-12 18:02:52 -0400

    fix placeholder styles

--- app/routes/transaction+/index.tsx
+++ app/routes/transaction+/index.tsx
@@ -306,9 +306,9 @@ export default function TransactionRoute() {
 						})}
 					</div>
 				) : (
-					<div className="no-scrollbar flex h-[calc(100%-4rem)] flex-col items-center justify-center  gap-2  rounded-md border-2 border-dashed text-xl font-semibold text-muted-foreground">
+					<div className=" no-scrollbar flex h-[calc(100%-4rem)] flex-col items-center justify-center gap-2  rounded-md  border-2 border-dashed  p-6 text-xl font-semibold text-muted-foreground">
 						<Icon className="text-2xl" name="scan" />
-						<h1>
+						<h1 className="text-balance text-center">
 							Ingrese el código de articulo para agregarlo a la transacción en
 							curso.
 						</h1>
