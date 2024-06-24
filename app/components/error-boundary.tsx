b7d6a201003767b92e0ae97405d81bbf2c5933ec
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-06-23 21:17:22 -0400

    fix styling

--- app/components/error-boundary.tsx
+++ app/components/error-boundary.tsx
@@ -38,7 +38,7 @@ export function GeneralErrorBoundary({
 	}
 
 	return (
-		<div className="container flex items-center justify-center p-20 text-h2">
+		<div className="container flex items-center justify-center p-20 text-h5 text-center ">
 			{isRouteErrorResponse(error)
 				? (statusHandlers?.[error.status] ?? defaultStatusHandler)({
 						error,
