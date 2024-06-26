fa7880c8b34505bdf29e9ac22f4922313b3d0c2d
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-06-26 17:37:17 -0400

    remake component

--- app/routes/order+/__productOrder+/__product-order-quantity.tsx
+++ app/routes/order+/__productOrder+/__product-order-quantity.tsx
@@ -1,100 +1,140 @@
-import { useFetcher } from '@remix-run/react'
-import { useEffect, useRef } from 'react'
-import { z } from 'zod'
 import { Button } from '#app/components/ui/button.tsx'
 import { Icon } from '#app/components/ui/icon.tsx'
 import { Input } from '#app/components/ui/input.tsx'
+import { useFetcher } from '@remix-run/react'
+import { useEffect, useRef, useState } from 'react'
+import { z } from 'zod'
+import { changeProductOrderQuantityActionType } from '../product-order'
 
-export const updateProductOrderQuantityActionIntent = 'update-po-quantity'
+export const increaseProductOrderQuantityActionIntent = 'increase-po-quantity'
+export const decreaseProductOrderQuantityActionIntent = 'decrease-po-quantity'
+export const changeProductOrderQuantityActionIntent = 'change-po-quantity'
 
-export const UpdateProductOrderQuantitySchema = z.object({
-	intent: z.literal(updateProductOrderQuantityActionIntent),
+export const IncreaseProductOrderQuantitySchema = z.object({
+	intent: z.literal(increaseProductOrderQuantityActionIntent),
+	productOrderId: z.string(),
+})
+export const DecreaseProductOrderQuantitySchema = z.object({
+	intent: z.literal(decreaseProductOrderQuantityActionIntent),
+	productOrderId: z.string(),
+})
+
+export const ChangeProductOrderQuantitySchema = z.object({
+	intent: z.literal(changeProductOrderQuantityActionIntent),
 	productOrderId: z.string(),
 	productOrderQuantity: z.number().min(0),
 })
 
-export const ProductOrderQuantity = ({
-	min,
-	max,
-	quantity,
-	setQuantity,
+export const ProductOrderQuantitySelector = ({
 	productOrderId,
+	quantity,
 }: {
-	min: number
-	max: number
-	quantity: number
-	setQuantity: (value: number) => void
 	productOrderId: string
+	quantity: number
 }) => {
-	const componentRef = useRef<HTMLDivElement>(null)
-
-	const productOrderQuantityFetcher = useFetcher({
-		key: `${updateProductOrderQuantityActionIntent}-${productOrderId}`,
+	const increaseProductOrderQuantityFetcher = useFetcher({
+		key: `${increaseProductOrderQuantityActionIntent}-${productOrderId}`,
 	})
-	const isSubmitting = productOrderQuantityFetcher.state !== 'idle'
 
-	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const newValue = parseInt(event.target.value, 10)
-		if (!isNaN(newValue) && newValue >= min && newValue <= max) {
-			setQuantity(newValue)
-		}
-	}
+	const decreaseProductOrderQuantityFetcher = useFetcher({
+		key: `${decreaseProductOrderQuantityActionIntent}-${productOrderId}`,
+	})
+	const changeProductOrderQuantityFetcher =
+		useFetcher<changeProductOrderQuantityActionType>({
+			key: `${changeProductOrderQuantityActionIntent}-${productOrderId}`,
+		})
 
-	//Apply update on change, after a short delay to allow the user to modify the amount
+	const [showInput, setShowInput] = useState(false)
+	const [shouldFocus, setShouldFocus] = useState(false)
+	const inputRef = useRef<HTMLInputElement>(null)
+
 	useEffect(() => {
-		productOrderQuantityFetcher.submit(
-			{
-				intent: updateProductOrderQuantityActionIntent,
-				productOrderId,
-				productOrderQuantity: quantity,
-			},
-			{
-				method: 'post',
-				action: '/order/product-order',
-			},
-		)
-	}, [quantity])
+		if (showInput && shouldFocus) {
+			inputRef.current?.focus()
+			inputRef.current?.select()
+			setShouldFocus(false)
+		}
+	}, [showInput, shouldFocus])
 
-	const increaseValue = () => {
-		const newValue = quantity < max ? quantity + 1 : quantity
-		setQuantity(newValue)
-	}
-
-	const decreaseValue = () => {
-		const newValue = quantity > min ? quantity - 1 : quantity
-		setQuantity(newValue)
-	}
+	const isSubmitting = [
+		increaseProductOrderQuantityFetcher,
+		decreaseProductOrderQuantityFetcher,
+		changeProductOrderQuantityFetcher,
+	].some(fetcher => fetcher.state !== 'idle')
 
 	return (
-		<div ref={componentRef} className="flex rounded-sm border border-muted-foreground/10">
-			<Button
-				tabIndex={-1}
-				variant={'ghost'}
+		<div className="flex  items-center rounded-sm border border-muted-foreground/10 p-0">
+			<decreaseProductOrderQuantityFetcher.Form
+				method="POST"
+				action="/order/product-order"
 				className="aspect-square h-[1.6rem] w-[2rem] rounded-sm p-0"
-				onClick={decreaseValue}
-				disabled={quantity <= min || isSubmitting}
 			>
-				<Icon size="lg" name="minus" />
-			</Button>
-
-			<Input
-				className="h-[1.6rem] w-[3rem] border-none p-0 text-center [&::-webkit-inner-spin-button]:appearance-none"
-				tabIndex={-1}
-				type="number"
-				value={quantity}
-				onChange={handleInputChange}
-				disabled={true}
-			/>
-
-			<Button
-				tabIndex={-1}
-				variant={'ghost'}
+				<input type="hidden" name="productOrderId" value={productOrderId} />
+				<Button
+					tabIndex={-1}
+					type="submit"
+					name="intent"
+					value={decreaseProductOrderQuantityActionIntent}
+					variant={'ghost'}
+					className="aspect-square h-[1.6rem] w-[2rem] rounded-sm p-0"
+					disabled={isSubmitting || quantity === 1}
+				>
+					<Icon size="lg" name="minus" />
+				</Button>
+			</decreaseProductOrderQuantityFetcher.Form>
+			<changeProductOrderQuantityFetcher.Form
+				className="appearance-none"
+				method="POST"
+				action="/order/product-order"
+				onSubmit={() => setShowInput(false)}
+				onBlur={() => setShowInput(false)}
+			>
+				<input type="hidden" name="productOrderId" value={productOrderId} />
+				<input
+					type="hidden"
+					name="intent"
+					value={changeProductOrderQuantityActionIntent}
+				/>
+				{showInput ? (
+					<Input
+						ref={inputRef}
+						className="h-[1.6rem] w-[3rem] border-none p-0 text-center [&::-webkit-inner-spin-button]:appearance-none"
+						tabIndex={-1}
+						type="number"
+						name="productOrderQuantity"
+						defaultValue={quantity}
+						disabled={isSubmitting}
+					/>
+				) : (
+					<div
+						className="flex h-[1.6rem] w-[3rem] cursor-pointer items-center justify-center rounded border-none p-0 text-center text-sm"
+						onClick={() => {
+							setShowInput(true)
+							setShouldFocus(true)
+						}}
+					>
+						{quantity}
+					</div>
+				)}
+			</changeProductOrderQuantityFetcher.Form>
+			<increaseProductOrderQuantityFetcher.Form
+				method="POST"
+				action="/order/product-order"
 				className="aspect-square h-[1.6rem] w-[2rem] rounded-sm p-0"
-				onClick={increaseValue}
-				disabled={quantity >= max || isSubmitting}
 			>
-				<Icon size="lg" name="plus" />
-			</Button>
+				<input type="hidden" name="productOrderId" value={productOrderId} />
+				<Button
+					tabIndex={-1}
+					variant={'ghost'}
+					className="aspect-square h-[1.6rem] w-[2rem] rounded-sm p-0"
+					type="submit"
+					name="intent"
+					value={increaseProductOrderQuantityActionIntent}
+					disabled={isSubmitting}
+				>
+					<Icon size="lg" name="plus" />
+				</Button>
+			</increaseProductOrderQuantityFetcher.Form>
 		</div>
 	)
 }
