97c94c161cef4361e2901c7fa09925e5638db307
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-07-17 11:59:17 -0400

    add todo comment

--- app/routes/_discounts+/_types/discount-type.ts
+++ app/routes/_discounts+/_types/discount-type.ts
@@ -5,6 +5,13 @@ export enum DiscountType {
 	FIXED = 'fixed',
 }
 
+//?TRY THIS INSTEAD
+// const DiscountType = {
+// 	PERCENTAGE: 'PERCENTAGE',
+// 	FIXED: 'FIXED',
+// } as const
+
+
 export const allDiscountTypes = [DiscountType.PERCENTAGE, DiscountType.FIXED] as const
 
 export const DiscountTypeSchema = z.enum(allDiscountTypes)
