3c5cace5052b354bf43aae4ba662e298cd168e28
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-03 11:04:35 -0400

    move to own file

---
+++ app/hooks/useRoveFocus.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { useMemo } from 'react'
+import { useHotkeys } from 'react-hotkeys-hook'
+import { Key } from 'ts-key-enum'
+
+export const useRoveFocus = (size: number) => {
+	const [currentFocus, setCurrentFocus] = useState(0)
+
+	useHotkeys(Key.ArrowDown, () =>
+		setCurrentFocus(currentFocus === size - 1 ? 0 : currentFocus + 1),
+	)
+	useHotkeys(Key.ArrowUp, () =>
+		setCurrentFocus(currentFocus === 0 ? size - 1 : currentFocus - 1),
+	)
+
+	return useMemo(() => [currentFocus, setCurrentFocus] as const, [currentFocus])
+}
