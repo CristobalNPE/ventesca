6cf296ef456e0995bbe82570bb83bb29499509f4
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-15 19:38:42 -0400

    move fn in

--- app/services/inventory/product-analytics.server.ts
+++ app/services/inventory/product-analytics.server.ts
@@ -1,4 +1,16 @@
-import { prisma } from "#app/utils/db.server.ts"
+import { OrderStatus } from '#app/types/orders/order-status.ts'
+import { ProductOrderType } from '#app/types/orders/productOrderType.ts'
+import { prisma } from '#app/utils/db.server.ts'
+import { capitalize } from '#app/utils/misc.tsx'
+import {
+	eachDayOfInterval,
+	endOfDay,
+	endOfWeek,
+	format,
+	startOfDay,
+	startOfWeek,
+} from 'date-fns'
+import { es } from 'date-fns/locale'
 
 export async function getBestSellingProduct(businessId: string) {
 	const bestSeller = await prisma.productAnalytics.findFirst({
@@ -51,4 +63,65 @@ export async function getMostProfitProduct(businessId: string) {
 	})
 
 	return mostProfit
+}
+
+export async function getCurrentWeekProductSales({
+	productId,
+	businessId,
+}: {
+	productId: string
+	businessId: string
+}) {
+	const weekDays = eachDayOfInterval({
+		start: startOfWeek(new Date(), { weekStartsOn: 1 }),
+		end: endOfWeek(new Date(), { weekStartsOn: 1 }),
+	})
+
+	const dailySales = await Promise.all(
+		weekDays.map(async (day) => {
+			const orders = await prisma.order.findMany({
+				where: {
+					businessId,
+					status: OrderStatus.FINISHED,
+					completedAt: { gte: startOfDay(day), lte: endOfDay(day) },
+				},
+				select: {
+					productOrders: {
+						where: {
+							productId,
+						},
+						select: { type: true, quantity: true },
+					},
+				},
+			})
+
+			const { totalSales, totalReturns } = orders.reduce(
+				(acc, order) => {
+					order.productOrders.forEach((po) => {
+						if (po.type === ProductOrderType.RETURN) {
+							acc.totalReturns += po.quantity
+						} else {
+							acc.totalSales += po.quantity
+						}
+					})
+					return acc
+				},
+				{ totalSales: 0, totalReturns: 0 },
+			)
+
+			return {
+				day: capitalize(
+					format(day, 'eeee', {
+						locale: es,
+					}),
+				),
+				date: format(day, 'dd/MM/yyyy', {
+					locale: es,
+				}),
+				totalSales,
+				totalReturns,
+			}
+		}),
+	)
+	return dailySales
 }
