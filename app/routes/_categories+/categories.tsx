96f0b672189910cc33f2036907e60928b61007d7
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-15 14:38:21 -0400

    refactor(categories): rename description to name
    - Update Category model to include 'name' field
    - Adjust related components and routes to use 'name' instead of 'description'
    - Update seed data and queries to reflect new schema
    - Modify UI to display category name in various components
    - Adjust inventory-related components to use category name
    - Update auth and user-related files for consistency

--- app/routes/_categories+/categories.tsx
+++ app/routes/_categories+/categories.tsx
@@ -1,34 +1,21 @@
+import { ContentLayout } from '#app/components/layout/content-layout.tsx'
+import { getBusinessId, requireUserId } from '#app/utils/auth.server.ts'
+import { prisma } from '#app/utils/db.server.ts'
+import { generateHexColor } from '#app/utils/misc.tsx'
 import { parseWithZod } from '@conform-to/zod'
 import { invariantResponse } from '@epic-web/invariant'
-import { type Category } from '@prisma/client'
 import {
 	type ActionFunctionArgs,
 	json,
 	type LoaderFunctionArgs,
 	redirectDocument,
-	type SerializeFrom,
 } from '@remix-run/node'
-import { Link, Outlet, useLoaderData, useLocation } from '@remix-run/react'
-import { endOfWeek, startOfWeek } from 'date-fns'
+import { useLoaderData } from '@remix-run/react'
 import { z } from 'zod'
-import { ContentLayout } from '#app/components/layout/content-layout.tsx'
-import { Spacer } from '#app/components/spacer.tsx'
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from '#app/components/ui/card.tsx'
-import { LinkWithParams } from '#app/components/ui/link-params.tsx'
-import { getBusinessId, requireUserId } from '#app/utils/auth.server.ts'
-import { prisma } from '#app/utils/db.server.ts'
-import { cn, formatCurrency, generateHexColor } from '#app/utils/misc.tsx'
 
+import { CategoryCard } from '#app/components/categories/category-card.tsx'
 import { requireUserWithRole } from '#app/utils/permissions.server.ts'
-import { userHasRole, useUser } from '#app/utils/user.ts'
-import { OrderStatus } from '../../types/orders/order-status.ts'
+import { useIsUserAdmin } from '#app/utils/user.ts'
 import {
 	createCategoryActionIntent,
 	CreateCategoryDialog,
@@ -39,21 +26,19 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request)
 	const businessId = await getBusinessId(userId)
 
-	const weeklyProfits = await getWeeklyProfitsPerCategory(businessId)
-
-	const categoryWithTopProfits = weeklyProfits[0]
-
 	const categories = await prisma.category.findMany({
 		where: { businessId },
 		select: {
 			id: true,
 			code: true,
+			colorCode: true,
+			name: true,
 			description: true,
 			_count: { select: { products: true } },
 		},
 	})
 
-	return json({ categories, categoryWithTopProfits })
+	return json({ categories })
 }
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -72,201 +57,23 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function CategoriesRoute() {
-	const user = useUser()
-	const isAdmin = userHasRole(user, 'Administrador')
+	const isAdmin = useIsUserAdmin()
 
-	const { categories, categoryWithTopProfits } = useLoaderData<typeof loader>()
+	const { categories } = useLoaderData<typeof loader>()
 
 	return (
 		<ContentLayout
 			title="Categorías"
 			actions={isAdmin && <CreateCategoryDialog />}
 		>
-			<main className="grid gap-y-4 lg:grid-cols-2 lg:gap-4">
-				<div className="flex flex-col gap-4">
-					{categoryWithTopProfits ? (
-						<Card>
-							<CardHeader className="pb-2">
-								<CardDescription>Mayores ingresos esta semana</CardDescription>
-								<CardTitle className="flex  items-center justify-between text-3xl">
-									<Link to={categoryWithTopProfits.id} className=" text-2xl">
-										{categoryWithTopProfits.description}
-									</Link>
-									<span className="text-muted-foreground">
-										{formatCurrency(categoryWithTopProfits.totalProfit)}
-									</span>
-								</CardTitle>
-							</CardHeader>
-							<CardContent></CardContent>
-						</Card>
-					) : null}
-					<CategoriesCard categories={categories} />
-				</div>
+			{/* TODO: Add a search bar */}
+			<main className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+				{categories.map((category) => (
+					<CategoryCard key={category.id} category={category} />
+				))}
 			</main>
-			{/* <main className=" h-full">
-	
-				<div className="grid h-[85dvh]  items-start gap-4 lg:grid-cols-3 ">
-					<div className="flex h-full flex-1 flex-col gap-4 overflow-hidden lg:col-span-1">
-						{categoryWithTopProfits ? (
-							<Card>
-								<CardHeader className="pb-2">
-									<CardDescription>Mayores ingresos esta semana</CardDescription>
-									<CardTitle className="flex  items-center justify-between text-3xl">
-										<Link to={categoryWithTopProfits.id} className=" text-2xl">
-											{categoryWithTopProfits.description}
-										</Link>
-										<span className="text-muted-foreground">
-											{formatCurrency(categoryWithTopProfits.totalProfit)}
-										</span>
-									</CardTitle>
-								</CardHeader>
-								<CardContent></CardContent>
-								{isAdmin ? (
-									<CardFooter>
-										<CreateCategoryDialog />
-									</CardFooter>
-								) : null}
-							</Card>
-						) : isAdmin ? (
-							<Card>
-								<CardHeader className="pb-2">
-								
-								</CardHeader>
-								<CardContent></CardContent>
-							</Card>
-						) : null}
-						<CategoriesCard categories={categories} />
-					</div>
-					<div className="lg:col-span-2">
-						<Outlet />
-					</div>
-				</div>
-			</main> */}
 		</ContentLayout>
 	)
-}
-
-function CategoriesCard({
-	categories,
-}: {
-	categories: SerializeFrom<
-		Pick<Category, 'id' | 'code' | 'description'> & {
-			_count: { products: number }
-		}
-	>[]
-}) {
-	const location = useLocation()
-
-	if (categories.length === 0) {
-		return (
-			<div className="flex h-full items-center justify-center rounded-sm bg-card text-muted-foreground">
-				<p>Aun no existen categorías registradas en sistema.</p>
-			</div>
-		)
-	}
-
-	return (
-		<Card className="no-scrollbar relative  h-full flex-grow overflow-y-auto">
-			<CardHeader className="sticky top-0 z-10 bg-card px-7">
-				<CardTitle>Categorías registradas</CardTitle>
-				<CardDescription>
-					Actualmente existen {categories.length} categorías registradas en
-					sistema.
-				</CardDescription>
-			</CardHeader>
-			<CardContent className="flex flex-col gap-1 ">
-				{categories.map(category => (
-					<LinkWithParams
-						key={category.id}
-						prefetch={'intent'}
-						className={({ isActive }) =>
-							cn(
-								'flex flex-wrap items-center justify-between gap-2 rounded-sm border-2 border-l-8 border-transparent border-b-secondary/30 border-l-secondary/80 p-2 text-sm transition-colors hover:bg-secondary ',
-								isActive && 'border-primary/10 bg-secondary',
-							)
-						}
-						preserveSearch
-						to={category.id}
-					>
-						<span className="sm:w-[5rem]  text-nowrap font-semibold">
-							{category.code}
-						</span>
-
-						<span className="sm:min-w-[15rem] flex-1 text-nowrap  text-start  text-muted-foreground">
-							{category.description}
-						</span>
-						<span className="sm:w-[10rem] text-nowrap  text-end  text-muted-foreground">
-							<span className="font-bold">{category._count.products}</span>{' '}
-							{category._count.products === 1 ? 'producto' : 'productos'}
-						</span>
-					</LinkWithParams>
-				))}
-			</CardContent>
-		</Card>
-	)
-}
-
-async function getWeeklyProfitsPerCategory(businessId: string) {
-	let startDate = new Date()
-
-	const currentWeekStartDate = startOfWeek(startDate)
-	const currentWeekEndDate = endOfWeek(startDate)
-
-	const productOrders = await prisma.productOrder.findMany({
-		where: {
-			order: { businessId },
-			createdAt: {
-				gte: currentWeekStartDate,
-				lte: currentWeekEndDate,
-			},
-		},
-		include: {
-			productDetails: {
-				include: {
-					category: true,
-				},
-			},
-			order: true,
-		},
-	})
-
-	type CategoryProfit = {
-		category: Category
-		totalProfit: number
-	}
-
-	const categoryProfits: { [key: string]: CategoryProfit } =
-		productOrders.reduce(
-			(acc, productOrder) => {
-				const { productDetails, totalPrice, order } = productOrder
-				const profit = order.status === OrderStatus.FINISHED ? totalPrice : 0
-
-				if (!acc[productDetails.category.id]) {
-					acc[productDetails.category.id] = {
-						category: productDetails.category,
-						totalProfit: 0,
-					}
-				}
-				let cat = acc[productDetails.category.id]
-
-				if (cat !== undefined) {
-					cat.totalProfit += profit
-				}
-
-				return acc
-			},
-			{} as { [key: string]: CategoryProfit },
-		)
-
-	const result = Object.values(categoryProfits).map(
-		({ category, totalProfit }) => ({
-			...category,
-			totalProfit,
-		}),
-	)
-	result.sort((a, b) => b.totalProfit - a.totalProfit)
-
-	return result
 }
 
 async function createCategoryAction(formData: FormData, businessId: string) {
@@ -296,13 +103,14 @@ async function createCategoryAction(formData: FormData, businessId: string) {
 		)
 	}
 
-	const { code, description } = submission.value
+	const { code, name } = submission.value
 
 	const createdCategory = await prisma.category.create({
 		data: {
+			name,
 			colorCode: generateHexColor(),
 			code,
-			description,
+			description: `Agrupa todos los productos relacionados con '${name}'`,
 			business: { connect: { id: businessId } },
 		},
 	})
