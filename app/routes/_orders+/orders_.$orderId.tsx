6ce148c37af55beeed79a1a66d2f66fbce2c53a5
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-15 12:14:18 -0400

    remove commented code

--- app/routes/_orders+/orders_.$orderId.tsx
+++ app/routes/_orders+/orders_.$orderId.tsx
@@ -7,14 +7,12 @@ import { ContentLayout } from '#app/components/layout/content-layout.tsx'
 import { OrderActions } from '#app/components/orders/order-actions.tsx'
 
 import { OrderDetails } from '#app/components/orders/order-details.tsx'
-import { OrderHeader } from '#app/components/orders/order-header.tsx'
 import { OrderProductsTable } from '#app/components/orders/order-products-table.tsx'
 import { OrderReceiptDisplay } from '#app/components/orders/order-receipt-display.tsx'
 import { OrderProvider } from '#app/context/orders/OrderContext.tsx'
 import { getBusinessId, requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { cn } from '#app/utils/misc.tsx'
-import { useIsUserAdmin } from '#app/utils/user.ts'
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request)
@@ -52,14 +50,15 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
 export default function OrderRoute() {
 	const loaderData = useLoaderData<typeof loader>()
-	const isAdmin = useIsUserAdmin()
 	const shouldShowReceipt = loaderData.order.productOrders.length > 0
 
 	return (
 		<OrderProvider data={loaderData}>
-			<ContentLayout title={`Transacción #${loaderData.order.id.slice(-6).toUpperCase()}`} limitHeight>
+			<ContentLayout
+				title={`Transacción #${loaderData.order.id.slice(-6).toUpperCase()}`}
+				limitHeight
+			>
 				<main className="flex h-full flex-col gap-4">
-					{/* <OrderHeader /> */}
 					<div
 						className={cn(
 							'grid flex-1 grid-cols-1 gap-y-4 lg:grid-cols-6 lg:gap-4  ',
@@ -70,7 +69,10 @@ export default function OrderRoute() {
 							<OrderDetails />
 						</div>
 						<div
-							className={cn('col-span-4 ', shouldShowReceipt && 'lg:col-span-3')}
+							className={cn(
+								'col-span-4 ',
+								shouldShowReceipt && 'lg:col-span-3',
+							)}
 						>
 							<OrderProductsTable />
 						</div>
