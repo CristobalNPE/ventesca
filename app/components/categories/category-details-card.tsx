d09f7a15408d04ee0eda5311d9f18692d3136102
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-17 20:18:59 -0400

    rename file

--- app/components/categories/category-details-card.tsx
+++ app/components/categories/category-details-card.tsx
@@ -13,16 +13,21 @@ import { CardContentItem } from '#app/components/card-content-item.tsx'
 import { Icon } from '#app/components/ui/icon.tsx'
 import { useCategory } from '#app/context/categories/CategoryContext.tsx'
 import { CategoryColorIndicator } from './category-color-indicator'
+import { Button } from '../ui/button'
+import { Link } from '@remix-run/react'
 
-export function CategoryDetails() {
+export function CategoryDetailsCard({ isAdmin }: { isAdmin: boolean }) {
 	const { category } = useCategory()
+
+	const canEdit = isAdmin && !category.isEssential
+
 	return (
-		<Card>
+		<Card className='min-h-[27rem] flex flex-col '>
 			<CardHeader className="flex flex-row items-center justify-between ">
 				<CardTitle>Detalles de la categoría</CardTitle>
 				<CategoryColorIndicator colorCode={category.colorCode} />
 			</CardHeader>
-			<CardContent className="grid gap-6">
+			<CardContent className="grid gap-6 flex-1">
 				<CardContentItem
 					icon={'hash'}
 					title={'Código'}
@@ -42,13 +47,20 @@ export function CategoryDetails() {
 					})}
 				/>
 			</CardContent>
-			<CardFooter className="flex  flex-row items-center justify-end border-t bg-muted/50 p-3 text-sm text-muted-foreground">
-				<span className="flex items-center gap-1">
+			<CardFooter className="flex  flex-row items-center justify-between gap-4 border-t bg-muted/50 py-3 px-6  text-muted-foreground">
+				<span className="flex items-center gap-1 text-xs">
 					<Icon size="sm" name="clock" /> Última modificación{' '}
 					{formatRelative(subDays(category.updatedAt, 0), new Date(), {
 						locale: es,
 					})}
 				</span>
+				{canEdit && (
+					<Button size={'sm'} asChild>
+						<Link to={`edit`} unstable_viewTransition>
+							<Icon name="pencil-2">Modificar datos</Icon>
+						</Link>
+					</Button>
+				)}
 			</CardFooter>
 		</Card>
 	)
