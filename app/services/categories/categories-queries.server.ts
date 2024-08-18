cd03e8440d4fad6863cb3e8be22b5e7ff22c6663
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-18 16:38:24 -0400

    fix get default category

--- app/services/categories/categories-queries.server.ts
+++ app/services/categories/categories-queries.server.ts
@@ -1,4 +1,5 @@
 import { prisma } from '#app/utils/db.server.ts'
+import { generateHexColor } from '#app/utils/misc.tsx'
 
 export async function getBusinessCategories(businessId: string) {
 	return await prisma.category.findMany({
@@ -6,5 +7,27 @@ export async function getBusinessCategories(businessId: string) {
 			businessId,
 		},
 		select: { name: true, id: true },
+	})
+}
+
+export async function getDefaultCategory(businessId: string) {
+	const defaultCategory = await prisma.category.findFirst({
+		where: { businessId, isEssential: true },
+	})
+
+	if (defaultCategory) {
+		return defaultCategory
+	}
+
+	return await prisma.category.create({
+		data: {
+			colorCode: generateHexColor(),
+			code: 0,
+			name: 'General',
+			description:
+				'Productos que no pertenecen a una categoría específica, o se encuentran sin clasificar.',
+			business: { connect: { id: businessId } },
+			isEssential: true,
+		},
 	})
 }
