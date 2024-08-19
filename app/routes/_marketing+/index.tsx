7bfe7ebbc3e8f68c6892957e5d53803d391223c2
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-19 09:14:19 -0400

    redirect based on user role

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -6,12 +6,24 @@ import {
 import { Link } from '@remix-run/react'
 import { Button } from '#app/components/ui/button.tsx'
 import { getUserId } from '#app/utils/auth.server.ts'
+import { requireUserWithRole } from '#app/utils/permissions.server.ts'
+import { prisma } from '#app/utils/db.server.ts'
 
 export const meta: MetaFunction = () => [{ title: 'Sistema de Ventas' }]
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const userId = await getUserId(request)
+
 	if (userId) {
+		const user = await prisma.user.findUnique({
+			where: {
+				id: userId,
+			},
+			select: { roles: { select: { name: true } } },
+		})
+		if (user?.roles.some((role) => role.name === 'admin')) {
+			throw redirect('/business')
+		}
 		throw redirect('/pos')
 	}
 	return null
