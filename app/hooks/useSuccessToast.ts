9257bd5eb2588baacfe123e03ea4ce9397721c51
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-08-25 13:02:40 -0400

    add reset fetcher

--- app/hooks/useSuccessToast.ts
+++ app/hooks/useSuccessToast.ts
@@ -1,4 +1,5 @@
-import { Fetcher } from '@remix-run/react'
+import { resetFetcher } from '#app/utils/resetFetcher.ts'
+import { FetcherWithComponents } from '@remix-run/react'
 import { useEffect } from 'react'
 import { toast } from 'sonner'
 
@@ -6,12 +7,13 @@ export function useSuccessToast({
 	fetcher,
 	message,
 }: {
-	fetcher: Fetcher
+	fetcher: FetcherWithComponents<any>
 	message: string
 }) {
 	useEffect(() => {
 		if (fetcher.data?.result.status === 'success' && fetcher.state === 'idle') {
 			toast.success(message)
+			resetFetcher(fetcher);
 		}
 	}, [fetcher.data, fetcher.state, message])
 }
